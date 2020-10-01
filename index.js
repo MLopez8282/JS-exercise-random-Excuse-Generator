83fc400d7b62c553d5f262a024a816ea3a8d245b
MLopez8282 <saltairpix@gmail.com>
2020-10-01 12:45:32 +0000

    background

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-let who = ['The dog ','My grandma ','His turtle ','My bird '];
-let action = ['ate ','peed ','crushed ','broke '];
+let who = ['The dog ', 'My grandma ', 'His turtle ', 'My bird '];
+let action = ['ate ', 'peed ', 'crushed ', 'broke '];
 let what = ['my homework ', 'the keys ', 'the car '];
-let when = ['before the class ','right on time ','when I finished ','during my lunch ','while I was praying '];
+let when = ['before the class ', 'right on time ', 'when I finished ', 'during my lunch ', 'while I was praying '];
 
 
-function randomNumber(array){
+function randomNumber(array) {
 
-   return Math.floor(Math.random() * array.length);
+    return Math.floor(Math.random() * array.length);
 
 }
 document.querySelector("#excuse").innerHTML = who[randomNumber(who)] + action[randomNumber(action)] + what[randomNumber(what)] + when[randomNumber(when)];
 
- console.log(randomNumber);
+console.log(randomNumber);
 
 
-/*randomNumber(when)
-console.log(when[randomNumber(when)]);*/
+
 
